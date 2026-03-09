532225cff204bb6a111ff01415b276f7459134f0
msmraqeeb <msmraqeeb@gmail.com>
2026-03-09 15:22:41 +0600

    1.0.3

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -1,4 +1,5 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import https from 'https';
 
 const isBuildTime = process.env.NEXT_PHASE === 'phase-production-build';
 
@@ -12,4 +13,9 @@ export const api = new WooCommerceRestApi({
     consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET || "cs_dummy",
     version: "wc/v3",
     queryStringAuth: true,
+    axiosConfig: {
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    }
 });
